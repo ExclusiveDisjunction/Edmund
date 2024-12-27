d8a23eaf0ec6236aa37365b0ba04d6ff70c016db
exdisj <exdisj@outlook.com>
2024-12-26 21:17:49 -0500

    Added better support to the Payday view, including better error messages and functionality. Changed the macOS version to be a little bit heigher for more advanced functonality. Additionally changed terminology from "tenders" to "accounts" in both code and UI.

---
+++ Edmund/Views/Transactions/CreditCardTrans.swift
@@ -0,0 +1,18 @@
+//
+//  CreditCardTrans.swift
+//  Edmund
+//
+//  Created by Hollan on 12/26/24.
+//
+
+import SwiftUI
+
+struct CreditCardTrans: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditCardTrans()
+}
