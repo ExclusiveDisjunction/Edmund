5cf62c65336bcdea760b66c5c6e24924c1dca582
exdisj <exdisj@outlook.com>
2024-12-31 15:38:27 -0500

    Added icons and started on balances sheet.

---
+++ Edmund/Views/Transactions/BalanceSheet.swift
@@ -0,0 +1,18 @@
+//
+//  BalanceSheet.swift
+//  Edmund
+//
+//  Created by Hollan on 12/31/24.
+//
+
+import SwiftUI
+
+struct BalanceSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BalanceSheet()
+}
