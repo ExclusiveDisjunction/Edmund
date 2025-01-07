90cb50858ceec0b3948990906f318ef3512a0538
exdisj <exdisj@outlook.com>
2025-01-07 01:02:35 -0500

    Added tabs

--- Edmund/Views/Main View Parts/AccsAndCats.swift
+++ Edmund/Views/Main View Parts/AccsAndCats.swift
@@ -11,9 +11,32 @@ import SwiftData
 struct AccsAndCats : View {
     @Query var accs: [AccountPair];
     @Query var cats: [CategoryPair];
+    @State var show_accs: Bool = true;
     
     var body: some View {
-        Text("yo momma")
+        VStack {
+            HStack {
+                Button(action: {
+                    show_accs = true
+                }) {
+                    Text("Accounts").font(.headline).padding(5)
+                }.buttonStyle(.link).foregroundStyle(show_accs ? Color.accentColor : Color.primary).border(show_accs ? Color.accentColor : Color.primary, width: show_accs ? 2 : 0).cornerRadius(2)
+                Button(action: {
+                    show_accs = false
+                }) {
+                    Text("Categories").font(.headline).padding(5)
+                }.buttonStyle(.link).foregroundStyle(!show_accs ? Color.accentColor : Color.primary).border(!show_accs ? Color.accentColor : Color.primary, width: !show_accs ? 2 : 0).cornerRadius(2)
+            }
+            
+            if show_accs {
+                Text("Showing Accounts")
+                Spacer()
+            }
+            else {
+                Text("Showing Categories")
+                Spacer()
+            }
+        }.padding()
     }
 }
 
